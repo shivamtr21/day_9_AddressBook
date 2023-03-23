584edec89513a127f82f5b7b15d54c7d59c39bb4
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-23 23:21:16 +0530

    Contact form

---
+++ Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class Contact
+    {
+        public string firstName;
+        public string lastName;
+        public string mobileNumber;
+        public string address;
+        public string city;
+        public string state;
+        public string zip;
+        public string email;
+    }
+}
